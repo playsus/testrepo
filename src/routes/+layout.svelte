b4ea1288df268cf11bfbcba3fae74754066a5bc2
Козловець <playsus.user.official@gmail.com>
2023-05-03 12:12:53 +0300

    Commit
    Commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,5 @@
 import '../styles/main.css';
 import Header from '../components/header/Header.svelte';
 import Footer from '../components/footer/Footer.svelte';
-import {} from '$lib/sentry'
 </script>
 <div class="app"><noscript><div class="alert alert-warning" role="alert"><strong>Your browser does not support JavaScript.</strong></div></noscript><Header /><main><slot /></main><Footer /></div>
